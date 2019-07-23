03ea2c321e6fbf5c8088053f12e8095949519874
andrico <andrico1234@yahoo.co.uk>
2019-07-23 21:00:51 +0100

    starter code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
-import { SkillProvider, SkillTreeGroup, SkillTree } from "beautiful-skill-tree";
-import { motion, harmony, cataclysm } from './data/data';
-import theme from './data/theme';
 
 function App() {
   return (
     <div className="App">
-      <div style={{ width: "500px", height: '150px', margin: '0 auto' }}>
-        <img style={{ width: "500px" }} src={logo} alt="borderlands logo." />
+      <div style={{ width: "500px", height: "150px", margin: "0 auto" }}>
+        <img
+          style={{ width: "500px", height: "200px" }}
+          src={logo}
+          alt="borderlands logo."
+        />
       </div>
-      <SkillProvider>
-        <SkillTreeGroup theme={theme}>
-          {() => {
-            return (
-              <React.Fragment>
-                <SkillTree title="Motion" treeId="motion" data={motion} />
-                <SkillTree title="Harmony" treeId="harmony" data={harmony} />
-                <SkillTree title="Cataclysm" treeId="cataclysm" data={cataclysm} />
-              </React.Fragment>
-            );
-          }}
-        </SkillTreeGroup>
-      </SkillProvider>
     </div>
   );
 }
