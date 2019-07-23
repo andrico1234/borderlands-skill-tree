1ea6aad2361ca4ab16a0b496333726a938d459be
andrico <andrico1234@yahoo.co.uk>
2019-07-23 20:06:51 +0100

    simplify themes

--- src/data/theme.js
+++ src/data/theme.js
@@ -1,41 +1,16 @@
 const theme = {
-  backgroundColor: 'transparent',
-  border: '4px solid rgb(127,127,127)',
-  borderRadius: '4px',
-  primaryFont: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
-  primaryFontColor: 'white',
-  tree: {
-    backgroundColor: 'rgba(60, 60, 60, 0.8)',
-  },
+  border: "4px solid rgb(127,127,127)",
+  treeBackgroundColor: "rgba(60, 60, 60, 0.8)",
   heading: {
     font: "Homenaje, Impact, sans-serif"
   },
-  node: {
-    backgroundColor: 'rgb(60,60,60)',
-    overlayColor: 'white',
-    activeBackgroundColor: '#5dc83c',
-    hoverBorderColor: `linear-gradient(
+  nodeBackgroundColor: "rgb(60,60,60)",
+  nodeActiveBackgroundColor: "#5dc83c",
+  nodeHoverBorderColor: `linear-gradient(
       to right,
       #ffffff 0%,
       #ffffff 100%
-    )`,
-    hoverBorder: '4px solid',
-    iconNodeWidth: '64px',
-    mobile: {
-      textNodeHeight: '32px',
-      textNodeWidth: '108px',
-      fontSize: '14px',
-    },
-    desktop: {
-      textNodeHeight: '28px',
-      textNodeWidth: '144px',
-      fontSize: '16px',
-    },
-  },
-  edge: {
-    border: '1px solid white',
-  },
-}
+    )`
+};
 
 export default theme;
